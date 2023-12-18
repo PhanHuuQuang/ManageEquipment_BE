0ab0741c02bebe3aaa03fb14f738569988eb58dd
hqdat809 <dathq@eupgroup.net>
2023-12-18 11:19:21 +0700

    done test refresh token

--- src/main/java/com/example/manageequipment/controller/NotificationController.java
+++ src/main/java/com/example/manageequipment/controller/NotificationController.java
@@ -20,7 +20,9 @@ public class NotificationController {
     @PostMapping("/get-notification/{userId}")
     public ResponseEntity<List<NotificationDto>> getNotification(@PathVariable Long userId) {
         return new ResponseEntity<>(notificationService.getNotification(userId), HttpStatus.OK);
-    }    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
+    }
+
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
     @PostMapping("/read-notification/{notificationId}")
     public ResponseEntity<NotificationDto> readNotification(@PathVariable Long notificationId) {
         return new ResponseEntity<>(notificationService.readNotification(notificationId), HttpStatus.OK);
